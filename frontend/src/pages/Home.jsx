7e466f442e60ca09ab89b827111db0f5ee4f7f84
Magnus <69847549+Magnumoe@users.noreply.github.com>
2022-10-25 23:38:47 +0200

    added big calendar

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,14 +1,32 @@
 import React, { useEffect } from 'react';
 
+import { Calendar, momentLocalizer } from 'react-big-calendar';
+import moment from 'moment';
+import "react-big-calendar/lib/css/react-big-calendar.css";
+
+
+const localizer = momentLocalizer(moment);
+const myEventsList = [
+  {
+    title: 'My event',
+    start: new Date(),
+    end: new Date(),
+  },
+];
+
 const Home = (props) => {
   useEffect(() => {
     props.handlePageChange("Home");
   }, [props]);
   return (
     <main>
-      <p>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod
-      </p>
+      <Calendar
+        localizer={localizer}
+        events={myEventsList}
+        startAccessor="start"
+        endAccessor="end"
+        style={{ height: 500, width: 1000 }}
+      />
     </main>
   )
 }
