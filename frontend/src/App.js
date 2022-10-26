89c01056b24bd69f42529e94767444747cc29517
Magnus <69847549+Magnumoe@users.noreply.github.com>
2022-10-27 00:53:59 +0200

    tested lazy suspence loading

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,19 +1,19 @@
 // imported packages
-import React, { useState } from "react";
+import React, { useState, lazy, Suspense } from "react";
 import { Routes, Route } from "react-router-dom";
 
 // imported pages and components
 import Home from "./pages/Home";
-import About from "./pages/About";
-import Contact from "./pages/Contact";
-import NotFound from "./pages/NotFound";
-import Business from "./pages/Business";
-import Positions from "./pages/Positions";
-
 import Navbar from "./components/Navbar/Navbar";
 import Header from "./components/Header/Header";
-import Footer from "./components/Footer/Footer";
-  
+
+const About = lazy(() => import("./pages/About"));
+const Contact = lazy(() => import("./pages/Contact"));
+const NotFound = lazy(() => import("./pages/NotFound"));
+const Business = lazy(() => import("./pages/Business"));
+const Positions = lazy(() => import("./pages/Positions"));
+const Footer = lazy(() => import("./components/Footer/Footer"));
+
 
 const App = () => {
   const [navbarActive, setNavbarActive] = useState(false);
@@ -31,14 +31,16 @@ const App = () => {
     <div className="App">
       <Navbar navbarActive={navbarActive} setNavbarActive={handleNavbar} />
       <Header title={Page} handleHamburger={handleNavbar} />
-      <Routes>
-        <Route index path="/" element={<Home handlePageChange={handlePageChange} />} />
-        <Route path="/about" element={<About handlePageChange={handlePageChange} />} />
-        <Route path="/contact" element={<Contact handlePageChange={handlePageChange} />} />
-        <Route path="/*" element={<NotFound handlePageChange={handlePageChange} />} />
-        <Route path="/business" element={<Business handlePageChange={handlePageChange} />} />
-        <Route path="/positions" element={<Positions handlePageChange={handlePageChange} />} />
-      </Routes>
+      <Suspense fallback={<div>Loading...</div>}>
+        <Routes>
+          <Route index path="/" element={<Home handlePageChange={handlePageChange} />} />
+          <Route path="/about" element={<About handlePageChange={handlePageChange} />} />
+          <Route path="/contact" element={<Contact handlePageChange={handlePageChange} />} />
+          <Route path="/*" element={<NotFound handlePageChange={handlePageChange} />} />
+          <Route path="/business" element={<Business handlePageChange={handlePageChange} />} />
+          <Route path="/positions" element={<Positions handlePageChange={handlePageChange} />} />
+        </Routes>
+      </Suspense>
       <Footer />
     </div>
   );
